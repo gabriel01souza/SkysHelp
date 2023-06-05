1d637a70990080f26092b40f6981c9f0ae245395
Gabriel Cardoso de Souza <gabriel.cardoso@paytrack.com.br>
2023-06-05 18:46:32 -0300

    Adicionando hash provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './view/home/home.component';
 import { EmployeeComponent } from './view/employee/employee.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,12 @@ import { EmployeeComponent } from './view/employee/employee.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
